62ecaafc205107249f8082935de701dea23731fb
Joseph Lyons <josephtlyons@gmail.com>
2019-06-01 23:29:02 -0400

    More efficient

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,21 @@ use std::path::Path;
 
 fn main() {
     let mut item_vec: ItemVector = ItemVector::new();
-    let home_dir = dirs::home_dir()
-        .expect("Couldn't get home directory")
-        .to_str()
-        .expect("Couldn't parse home directory path to &str")
-        .to_string();
+    let home_dir_path_buf = dirs::home_dir().expect("Couldn't get home directory");
+    let home_dir_path = home_dir_path_buf.as_path();
 
-    get_items_from_default_paths(&mut item_vec, &home_dir);
-    get_items_from_user_defined_paths(&mut item_vec, home_dir);
+    get_items_from_default_paths(&mut item_vec, home_dir_path);
+    get_items_from_user_defined_paths(&mut item_vec, home_dir_path);
 
     item_vec.sort();
     item_vec.print();
 }
 
-fn get_items_from_default_paths(item_vec: &mut ItemVector, home_dir: &str) {
-    let default_paths = include_str!("default_paths.txt").lines();
+fn get_items_from_default_paths(item_vec: &mut ItemVector, home_dir: &Path) {
+    let paths = include_str!("default_paths.txt").lines();
 
-    for default_path in default_paths {
-        let assembled_path = Path::new(&home_dir).join(Path::new(default_path));
+    for path in paths {
+        let assembled_path = home_dir.join(Path::new(path));
 
         if assembled_path.exists() {
             item_vec.add_items_in_dir(&assembled_path);
@@ -33,12 +30,13 @@ fn get_items_from_default_paths(item_vec: &mut ItemVector, home_dir: &str) {
     }
 }
 
-fn get_items_from_user_defined_paths(item_vec: &mut ItemVector, home_dir: String) {
+fn get_items_from_user_defined_paths(item_vec: &mut ItemVector, home_dir: &Path) {
     // Code for retrieving extra user defined paths
-    let paths_file = File::open(
-        home_dir + "/Library/Application Support/The Lyons' Den Labs/shlogger_extra_paths.txt"
-    )
-    .expect("Couldn't open file or it does not exist.");
+    let path = home_dir.join(Path::new(
+        "Library/Application Support/The Lyons' Den Labs/shlogger_extra_paths.txt",
+    ));
+
+    let paths_file = File::open(path).expect("Couldn't open file or it does not exist.");
 
     let paths_file_buf = BufReader::new(paths_file);
 
