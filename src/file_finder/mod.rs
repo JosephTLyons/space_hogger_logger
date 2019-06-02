4a680d367cf6672ac2581f66bce5f97680f4884e
Joseph Lyons <josephtlyons@gmail.com>
2019-06-02 03:30:28 -0400

    Missed some 'item' replacements in last commit

--- src/file_finder/mod.rs
+++ src/file_finder/mod.rs
@@ -21,40 +21,40 @@ impl FileFinder {
     }
 
     fn recursively_get_files_in_dir(&mut self, path: &Path) {
-        let dir_file_iter: ReadDir = read_dir(path).expect("Couldn't obtain item iter.");
+        let dir_file_iter: ReadDir = read_dir(path).expect("Couldn't obtain file iter.");
 
-        for item in dir_file_iter {
-            let item = item.expect("Wasn't able to obtain item");
-            let item_path_buf = item.path();
-            let item_path = item_path_buf.as_path();
-            let item_metadata = item.metadata().expect("Couldn't get item's metadata");
+        for file in dir_file_iter {
+            let file = file.expect("Wasn't able to obtain file");
+            let file_path_buf = file.path();
+            let file_path = file_path_buf.as_path();
+            let file_metadata = file.metadata().expect("Couldn't get file's metadata");
 
-            if item_metadata.is_dir() {
-                self.recursively_get_files_in_dir(item_path);
+            if file_metadata.is_dir() {
+                self.recursively_get_files_in_dir(file_path);
             } else {
                 self.file_vec.push(FileObject {
-                    path: item_path
+                    path: file_path
                         .to_str()
                         .expect("Could not create &str file path")
                         .to_string(),
-                    size_in_bytes: item_metadata.len(),
+                    size_in_bytes: file_metadata.len(),
                 });
             }
         }
     }
 
     pub fn sort(&mut self) {
-        // item_vec.sort_by(|a, b| a.1.cmp(&b.1));
-        // item_vec.sort_by(|(_, u1), (_, u2)| u1.cmp(u2));
+        // file_vec.sort_by(|a, b| a.1.cmp(&b.1));
+        // file_vec.sort_by(|(_, u1), (_, u2)| u1.cmp(u2));
         self.file_vec.sort_unstable_by_key(|a| a.size_in_bytes);
     }
 
     pub fn print(&self) {
-        for item in &self.file_vec {
+        for file in &self.file_vec {
             println!(
                 "{}{}",
-                format!("{:>12}", convert(item.size_in_bytes as f64) + ": "),
-                item.path
+                format!("{:>12}", convert(file.size_in_bytes as f64) + ": "),
+                file.path
             );
         }
     }
